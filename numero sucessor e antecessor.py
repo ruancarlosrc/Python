22dbd77cfd4a0b1f6a6bf13481f31f942589c9bc
Ruan Carlos <ruancarlos.rc794@gmail.com>
2023-11-19 19:45:34 -0300

    diz qual o numero anterior e posterior ao digitado

---
+++ numero sucessor e antecessor.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um numero :"));
+anter = num - 1;
+poster = num + 1;
+
+print("O numero digitado foi:", num);
+print("E o seu antecessor é:", anter);
+print("E o seu posterior é:", poster);
