213082beba7274f0a1fe72992cc2cb9f4bfa9bc3
Ruan Carlos <ruancarlos.rc794@gmail.com>
2023-11-19 17:43:27 -0300

    le as informaçoes do usuario e mostra na tela

---
+++ dados pessoais.py
@@ -0,0 +1,11 @@
+nome = str(input("Digite seu nome: "));
+idade= int(input("Digite sua Idade: "));
+altura = float(input("Digite a sua altra: "))
+
+#considerando o ano atual como 2023
+ano = 2023 - idade
+
+print("Seu nome é:", nome ,".");
+print("Sua idade é:", idade ,".");
+print("Sua altura é:", altura ,".");
+print("Seu ano de nascimento é:", ano ,".");
