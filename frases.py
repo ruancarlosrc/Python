fab1bf626865257d50d988f4f91ac76a7885b754
Ruan Carlos <ruancarlos.rc794@gmail.com>
2023-11-22 21:53:33 -0300

    Um pouco sobre frases e trrabalhos com numeros

---
+++ frases.py
@@ -0,0 +1,8 @@
+phrase = "Academia Girafas"
+print(phrase);
+print (len(phrase))
+print(phrase[5]);
+print(phrase + " Para Todos");
+print(phrase.upper());
+print(phrase.index("d"));
+print(phrase.replace("Girafas","Tatu"));
---
+++ mexendo com numeros.py
@@ -0,0 +1,10 @@
+print(1);
+print(1.945);
+print(-1.945);
+print(2.5 + 5.5);
+print(50 / 11);
+print(8 * 15);
+print((66+18)/12*4);
+print((66+18)/(12*4));
+meu_numero = 8
+print(str(meu_numero) + " Este numero e uma string");
